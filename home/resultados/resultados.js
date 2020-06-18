73fa3e713639feb1f7e96ef0139e58a9b8096496
carlosdealvear <63105885+carlosdealvear@users.noreply.github.com>
2020-06-18 17:36:41 -0300

    D

--- home/resultados/resultados.js
+++ home/resultados/resultados.js
@@ -1,32 +1,86 @@
-let queryString = location.search; //Capturamos la query string del navegador
+let queryString = location.search; 
 console.log(queryString);
 
-let searchParams = new URLSearchParams(queryString); //Obtenemos un objeto literal con toda la info de los parametros en la url
-console.log(searchParams);
-
-let search = searchParams.get("search"); //con el método get obtenenemos el valor del término a buscar. En este obtenenemos lo que escribió el usuario en el campo de busqueda cuyo "name" es "search" (name="search").
+let queryStringObj = new URLSearchParams(queryString);
+console.log(queryStringObj);
+let search = queryStringObj.get('search'); 
 console.log(search);
-let proxy = 'https://cors-anywhere.herokuapp.com/';
-let url =  proxy + "https://api.deezer.com/search/artist?q=" + search;
 
+let proxy = "https://cors-anywhere.herokuapp.com/";
+let url =  proxy + 'https://api.deezer.com/search/artist?q=' + search;
+console.log(url);
 
 fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(datos){
-        //Resolver que hacemos con los datos.
         console.log(datos);
-        let lista = document.querySelector('.lista');
+        let lista = document.querySelector('.resultadoartistas');
         let resultados = datos.data;
         
         resultados.forEach(function(resultado){
-            lista.innerHTML += '<li>' + resultado.name+ '</li>'
+            lista.innerHTML += '<li><a class="result" href="detail.html?type=' + resultado.type + '&id=' + resultado.id + '">' + resultado.name + '</a></li>';
         });
-       
+       console.log(datos)
+
+       })
         
         
+
+    .catch(function(error){
+        console.log(error);
+        
+    });
+
+
+let urltrack =  proxy + 'https://api.deezer.com/search/track?q=' + search;
+console.log(urltrack);
+
+fetch(urltrack)
+    .then(function(response){
+        return response.json();
     })
+    .then(function(datos){
+        console.log(datos);
+        let lista = document.querySelector('.resultadocanciones');
+        let resultados = datos.data;
+        
+        resultados.forEach(function(resultado) {
+            lista.innerHTML += '<li>' + resultado.title + '</li>';
+        });
+       console.log(datos)
+
+       })
+        
+        
+
+    .catch(function(error){
+        console.log(error);
+        
+    });
+
+    let urlalbum =  proxy + 'https://api.deezer.com/search/album?q=' + search;
+console.log(urlalbum);
+
+fetch(urlalbum)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        let lista = document.querySelector('.resultadoalbum');
+        let resultados = datos.data;
+        
+        resultados.forEach(function(resultado){
+            lista.innerHTML += '<li><a class="result" href="detail.html?type=' + resultado.type + '&id=' + resultado.id + '">' + resultado.title + '</a></li>';
+        });
+       console.log(datos)
+
+       })
+        
+        
+
     .catch(function(error){
         console.log(error);
         
